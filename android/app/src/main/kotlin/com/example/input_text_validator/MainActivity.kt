8ca7673f539ed7580c4b5350c46b88186fc8a9fc
Safdar Nazir <safdarnazir14@gmail.com>
2020-12-27 00:23:57 +0500

    Working on Input Text Validator.

---
+++ android/app/src/main/kotlin/com/example/input_text_validator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.input_text_validator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/input_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:input_text_validator/screens/body.dart';
+
+class InputPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
